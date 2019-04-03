744bbc94d16a8fe08188fd6e56db31a9945f3125
昏头 <wangxing1014@gmail.com>
2019-04-03 14:04:07 +0800

    demo

--- src/Bep/DemoBepImpl.php
+++ src/Bep/DemoBepImpl.php
@@ -2,15 +2,29 @@
 
 namespace YouzanCloudBootApp\Bep;
 
+use ExtensionPointApi\Com\Youzan\Cloud\Extension\Param\Test\BizTestOutParam;
+use ExtensionPointApi\Com\Youzan\Cloud\Extension\Param\Test\BizTestRequest;
+use ExtensionPointApi\Com\Youzan\Cloud\Extension\Param\Test\BizTestResponse;
 use YouzanCloudBoot\ExtensionPoint\BaseBusinessExtensionPointImpl;
+use ExtensionPointApi\Com\Youzan\Cloud\Extension\Api\BizTestService;
 
-class DemoBepImpl extends BaseBusinessExtensionPointImpl
+class DemoBepImpl extends BaseBusinessExtensionPointImpl implements BizTestService
 {
 
-    public function test()
+    public function invoke(BizTestRequest $bizTestRequest): BizTestOutParam
     {
-        $this->getLog()->info("hello world test");
-        return ['hello' => 'world'];
-    }
+        // TODO: Implement invoke() method.
+        $this->getLog()->info($bizTestRequest->getData()->getContent());
+        $result = new BizTestOutParam();
+        $result->setCode("200");
+        $result->setMessage("call success");
+        $result->setSuccess(true);
 
+        $response = new BizTestResponse();
+        $response->setContent("test content response");
+        $response->setRequestId(100);
+        $result->setData($response);
+
+        return $result;
+    }
 }
