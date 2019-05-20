4dde941d1e9bc363865da4fca100624e8144515d
xu42 <xu42.cn@gmail.com>
2019-05-20 18:59:43 +0800

    Support View

--- config/routes.php
+++ config/routes.php
@@ -3,3 +3,8 @@
 /** @var \Slim\App $app */
 
 $app->get('/', \YouzanCloudBootApp\Controller\IndexController::class . ':index');
+
+/**
+ * 模版演示
+ */
+$app->get('/templates/example/{name}', \YouzanCloudBootApp\Controller\TemplateController::class . ':example');
