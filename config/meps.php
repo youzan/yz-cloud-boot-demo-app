127bfbe91b98c539a5c0130607d3af4696775027
xu42 <xu42.cn@gmail.com>
2019-12-04 17:16:42 +0800

    Add Test Mep

--- config/meps.php
+++ config/meps.php
@@ -4,4 +4,10 @@
 
 $mepReg->register('testTopic', \YouzanCloudBootApp\Mep\TestMsgHandler::class);
 
+$mepReg->register('ntc_order_create', \YouzanCloudBootApp\Mep\TestMsgHandler::class);
+$mepReg->register('ntc_order_pay', \YouzanCloudBootApp\Mep\TestMsgHandler::class);
+$mepReg->register('ntc_order_wait_ship', \YouzanCloudBootApp\Mep\TestMsgHandler::class);
+$mepReg->register('ntc_order_cancel', \YouzanCloudBootApp\Mep\TestMsgHandler::class);
+$mepReg->register('ntc_order_success', \YouzanCloudBootApp\Mep\TestMsgHandler::class);
+
 
